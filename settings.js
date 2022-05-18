f5973c9f7c911cf2fb8400258f721f0d3dc0a808
clayreCasa <105792593+clayreCasa@users.noreply.github.com>
2022-05-18 19:42:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['27634090203'] 
-global.pemilik = ['27634090203'] //pemilik nomor
-global.premium = ['27634090203'] //premium number
+global.owner = ['6285156147636'] 
+global.pemilik = ['6285156147636'] //pemilik nomor
+global.premium = ['6285156147636'] //premium number
 global.pengguna = 'Drips' //pengguna
 global.botnma = 'ZIM BOT INC' //botnama,botname
 global.ownernma = 'Drips' //ownernama,ownername
